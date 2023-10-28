a6cd9680539160c1aa21a9203360f2dc885d742a
Aaliya-Shanez <aaliyashanez786@gmail.com>
2023-10-28 15:41:15 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+void main()
+{
+  runApp(MaterialApp(home: Center(child: Text('Hello Flutter Workshop',style: TextStyle(color: Colors.lightGreen,fontSize:100,decoration: TextDecoration.none),)),));
+}
